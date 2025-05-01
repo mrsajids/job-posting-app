0507c210576a68c9f76f88b59f00a96e1ff291a5
Sajeed <syedsajeed6969@gmail.com>
2025-05-01 11:44:28 +0530

    img added

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom"
+import logo from "../images/logo.png"
 import "../assets/navbar.css"
 const Navbar = () => {
     return (
         <nav>
             <div class="nav-container">
                 <div class="logo">
-                    <img src="https://play-lh.googleusercontent.com/7y6gd1TKP4skySj-zI3KUs9uw9bpC99EOQNKa6BefSUHzzesFpIex3pDXWCCDTYywLM=w240-h480-rw" alt="Logo" />
+                    <img src={logo} alt="Logo" />
                 </div>
                 <ul class="nav-links">
                     <li ><Link className="nav-link" to="/">Home</Link></li>
